c713e418fdf3dab7dbcf86340da9c84e86c52d8a
Michel Pelletier <pelletier.michel@gmail.com>
2013-10-10 18:28:39 -0700

    reformatted zstr for docs.

--- pyczmq/zstr.py
+++ pyczmq/zstr.py
@@ -1,66 +1,59 @@
 from pyczmq._cffi import C, ffi, tostr
 
-functions = \
-'''
-/*  =========================================================================
-    zstr - sending and receiving strings
 
-    -------------------------------------------------------------------------
-    Copyright (c) 1991-2013 iMatix Corporation <www.imatix.com>
-    Copyright other contributors as noted in the AUTHORS file.
+ffi.cdef('char * zstr_recv (void *socket);')
+@tostr
+def recv(sock):
+    """
+    Receive a string off a socket, caller must free it
+    """
+    return C.zstr_recv(sock)
 
-    This file is part of CZMQ, the high-level C binding for 0MQ:
-    http://czmq.zeromq.org.
 
-    This is free software; you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License as published by the 
-    Free Software Foundation; either version 3 of the License, or (at your 
-    option) any later version.
+ffi.cdef('char * zstr_recv_nowait (void *socket);')
+@tostr
+def recv_nowait(sock):
+    """
+    Receive a string off a socket if socket had input waiting
+    """
+    return C.zstr_recv_nowait(sock)
 
-    This software is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
-    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General 
-    Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License 
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-    =========================================================================
-*/
+ffi.cdef('int zstr_send (void *socket, const char *format, ...);')
+def send(sock, fmt):
+    """
+    Send a formatted string to a socket
+    """
+    C.zstr_send(sock, fmt)
 
-//  @interface
-//  Receive a string off a socket, caller must free it
- char * zstr_recv (void *socket);
 
-//  Receive a string off a socket if socket had input waiting
- char * zstr_recv_nowait (void *socket);
+ffi.cdef('int zstr_sendm (void *socket, const char *format, ...);')
+def sendm(sock, fmt):
+    """
+    Send a formatted string to a socket, with MORE flag
+    """
+    return C.zstr_sendm(sock, fmt)
 
-//  Send a formatted string to a socket
- int zstr_send (void *socket, const char *format, ...);
 
-//  Send a formatted string to a socket, with MORE flag
- int zstr_sendm (void *socket, const char *format, ...);
+ffi.cdef('int zstr_sendx (void *socket, const char *string, ...);')
+def sendx(sock, string):
+    """
+    Send a series of strings (until NULL) as multipart data
+    Returns 0 if the strings could be sent OK, or -1 on error.
+    """
+    return C.zstr_sendx(sock, string)
 
-//  Send a series of strings (until NULL) as multipart data
-//  Returns 0 if the strings could be sent OK, or -1 on error.
- int zstr_sendx (void *socket, const char *string, ...);
 
-//  Receive a series of strings (until NULL) from multipart data
-//  Each string is allocated and filled with string data; if there
-//  are not enough frames, unallocated strings are set to NULL.
-//  Returns -1 if the message could not be read, else returns the
-//  number of strings filled, zero or more.
- int zstr_recvx (void *socket, char **string_p, ...);
+ffi.cdef('int zstr_recvx (void *socket, char **string_p, ...);')
+def recvx(sock, string_p):
+    """
+    Receive a series of strings (until NULL) from multipart data
+    Each string is allocated and filled with string data; if there
+    are not enough frames, unallocated strings are set to NULL.
+    Returns -1 if the message could not be read, else returns the
+    number of strings filled, zero or more.
+    """
+    return C.zstr_recvx(sock, string_p)
 
-//  Self test of this class
- int zstr_test (bool verbose);
-'''
 
-ffi.cdef(functions)
-
-recv = tostr(C.zstr_recv)
-recv_nowait = tostr(C.zstr_recv_nowait)
-send = C.zstr_send
-sendm = C.zstr_sendm
-sendx = C.zstr_sendx
-recvx = C.zstr_recvx
-test = C.zstr_test
+ffi.cdef('int zstr_test (bool verbose);')
