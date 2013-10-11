5a44badf253b8c712990f3ca484451ab2d146318
Michel Pelletier <pelletier.michel@gmail.com>
2013-10-10 19:01:28 -0700

    reformat zbeacon

--- pyczmq/zbeacon.py
+++ pyczmq/zbeacon.py
@@ -1,92 +1,75 @@
 from pyczmq._cffi import C, ffi, ptop, tostr
 
-ffi.cdef('''
-/*  =========================================================================
-    zbeacon - LAN service announcement and discovery
-    
-    -------------------------------------------------------------------------
-    Copyright (c) 1991-2013 iMatix Corporation <www.imatix.com>
-    Copyright other contributors as noted in the AUTHORS file.
 
-    This file is part of CZMQ, the high-level C binding for 0MQ:
-    http://czmq.zeromq.org.
+ffi.cdef('typedef struct _zbeacon_t zbeacon_t;')
 
-    This is free software; you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License as published by the 
-    Free Software Foundation; either version 3 of the License, or (at your 
-    option) any later version.
 
-    This software is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
-    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General 
-    Public License for more details.
+ffi.cdef('void zbeacon_destroy (zbeacon_t **self_p);')
+def destroy(beacon):
+    """Destroy a beacon
+    """
+    C.zbeacon_destroy(ptop('zbeacon_t', beacon))
 
-    You should have received a copy of the GNU Lesser General Public License 
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-    =========================================================================
-*/
 
-//  Opaque class structure
-typedef struct _zbeacon_t zbeacon_t;
-
-//  @interface
-//  Create a new beacon on a certain UDP port
- zbeacon_t *
-    zbeacon_new (int port_nbr);
-    
-//  Destroy a beacon
- void
-    zbeacon_destroy (zbeacon_t **self_p);
-
-//  Return our own IP address as printable string
- char *
-    zbeacon_hostname (zbeacon_t *self);
-
-//  Set broadcast interval in milliseconds (default is 1000 msec)
- void
-    zbeacon_set_interval (zbeacon_t *self, int interval);
-
-//  Filter out any beacon that looks exactly like ours
- void
-    zbeacon_noecho (zbeacon_t *self);
-
-//  Start broadcasting beacon to peers at the specified interval
- void
-    zbeacon_publish (zbeacon_t *self, char *transmit, size_t size);
-    
-//  Stop broadcasting beacons
- void
-    zbeacon_silence (zbeacon_t *self);
-
-//  Start listening to other peers; zero-sized filter means get everything
- void
-    zbeacon_subscribe (zbeacon_t *self, char *filter, size_t size);
-
-//  Stop listening to other peers
- void
-    zbeacon_unsubscribe (zbeacon_t *self);
-
-//  Get beacon ZeroMQ socket, for polling or receiving messages
- void *
-    zbeacon_socket (zbeacon_t *self);
-
-//  Self test of this class
- void
-    zbeacon_test (bool verbose);
-''')
-
+ffi.cdef('zbeacon_t * zbeacon_new (int port_nbr);')
 def new(port):
-    beacon = C.zbeacon_new(port)
-    def destroy(c):
-        C.zbeacon_destroy(ptop('zbeacon_t', c))
-    return ffi.gc(beacon, destroy)
+    """Create a new beacon on a certain UDP port
+    """
+    return ffi.gc(C.zbeacon_new(port), destroy)
 
-hostname = tostr(C.zbeacon_hostname)
-interval = C.zbeacon_set_interval
-noecho = C.zbeacon_noecho
-publish = C.zbeacon_publish
-silence = C.zbeacon_silence
-subscribe = C.zbeacon_subscribe
-unsubscribe = C.zbeacon_unsubscribe
-socket = C.zbeacon_socket
-test = C.zbeacon_test
+
+ffi.cdef('char * zbeacon_hostname (zbeacon_t *self);')
+@tostr
+def hostname(beacon):
+    return C.zbeacon_hostname(beacon)
+
+
+ffi.cdef('void zbeacon_set_interval (zbeacon_t *self, int interval);')
+def interval(beacon, interval):
+    """Set broadcast interval in milliseconds (default is 1000 msec)
+    """
+    return C.zbeacon_set_interval(beacon, interval)
+
+
+ffi.cdef('void zbeacon_noecho (zbeacon_t *self);')
+def noecho(beacon):
+    """Filter out any beacon that looks exactly like ours
+    """
+    return C.zbeacon_noecho(beacon)
+
+
+ffi.cdef('void zbeacon_publish (zbeacon_t *self, char *transmit, size_t size);')
+def publish(beacon, string):
+    """Start broadcasting beacon to peers at the specified interval"""
+    return C.zbeacon_publish(beacon, string, len(string))
+
+
+ffi.cdef('void zbeacon_silence (zbeacon_t *self);')
+def silence(beacon):
+    """Stop broadcasting beacons
+    """
+    return C.zbeacon_silence(beacon)
+
+
+ffi.cdef('void zbeacon_subscribe (zbeacon_t *self, char *filter, size_t size);')
+def subscribe(beacon, string):
+    """Start listening to other peers; zero-sized filter means get everything
+    """
+    return C.zbeacon_subscribe(beacon, string, len(string))
+
+
+ffi.cdef('void zbeacon_unsubscribe (zbeacon_t *self);')
+def unsubscribe(beacon):
+    """Stop listening to other peers
+    """
+    return C.zbeacon_unsubscribe(beacon)
+
+
+ffi.cdef('void * zbeacon_socket (zbeacon_t *self);')
+def socket(beacon):
+    """Get beacon ZeroMQ socket, for polling or receiving messages
+    """
+    return C.zbeacon_socket(beacon)
+
+
+ffi.cdef('void zbeacon_test (bool verbose);')
