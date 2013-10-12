d7e6b7cf52be857e322c98cdcba11ed0e31124ed
Michel Pelletier <pelletier.michel@gmail.com>
2013-10-12 15:18:14 -0700

    bump 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="pyczmq",
-  version="0.0.2",
+  version="0.0.3",
   packages=find_packages(exclude=['tests.*', 'tests', '.virt']),
 
   tests_require=['nose'],
