a7bfee7adecb1944057ea07963d0accb1a460acf
Chris Laws <clawsicus@gmail.com>
2013-11-30 09:14:12 +1030

    Fixes supporting Travis CI test builds

--- pyczmq/__init__.py
+++ pyczmq/__init__.py
@@ -1,4 +1,4 @@
-from _cffi import C, ffi
+from ._cffi import C, ffi
 
 from . import (
     zmq,
@@ -15,6 +15,6 @@ from . import (
     zcertstore,
     )
 
-from types import Context, Socket, Frame, Message, Loop, Beacon
+from .types import Context, Socket, Frame, Message, Loop, Beacon
 
 
